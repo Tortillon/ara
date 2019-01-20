13bb11cf289d418d3f56cf991b1c9651bd8d5fa8
delipl <delicat.kuba@gmail.com>
2019-01-20 23:47:53 +0100

    pokoloruj

--- main.cpp
+++ main.cpp
@@ -243,11 +243,11 @@ int main()
                                 {
                                     for(int k = 0; k < 17; k++){
                                         for (int l = 0; l < 34; l++){
-                                            if(canAttack(wsk_to_board), xx, yy, k, l){
+                                            if(canAttack(front_fields, xx, yy, k, l)){
                                                 //pokoloruj pole wsk_to_board[(xx+k)*34 + yy] na czerwono;
                                                 front_fields[34*figure_x+figure_y].setColor(sf::Color::Red);
                                             }
-                                            if(canMove(wsk_to_board), xx, yy, k, l){
+                                            if(canMove(front_fields, xx, yy, k, l){
                                                 //pokoloruj pole wsk_to_board[(xx+k)*34 + yy] na pomaranczowo;
                                                 front_fields[34*figure_x+figure_y].setColor(sf::Color::Orange); 
                                             }     
